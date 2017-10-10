a1ae7d85fd91132bfe5e05a396465cc801b5fb8b
uufree <369470777@qq.com>
2017-10-10 16:42:48 +0800

    add stdiny

---
+++ test.cc
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: test.cc
+	> Author: uuchen
+	> Mail: 1319081676@qq.com
+	> Created Time: 2017年10月10日 星期二 16时38分56秒
+ ************************************************************************/
+
+#include<iostream>
+
+int main(int argc,char** argv)
+{
+    std::cout << sizeof(signed long) << std::endl;
+
+    return 0;
+}
+
