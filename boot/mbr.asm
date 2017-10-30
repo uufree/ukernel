36aee206df5f2ca93e3f116e16ee7c4310df6fb0
uufree <369470777@qq.com>
2017-10-30 19:39:30 +0800

    fix loader.asm finish

--- boot/mbr.asm
+++ boot/mbr.asm
@@ -31,26 +31,69 @@ section mbr vstart=0x7c00
     loop @g
     
 
-;    mov eax,LOADER_START_SECTOR
-;    mov bx,LOADER_BASE_ADDR
-;    mov cx,4
-;    call readDisk
-;    jmp LOADER_BASE_ADDR + 0x300
+    mov eax,LOADER_START_SECTOR
+    mov bx,LOADER_BASE_ADDR
+    mov cx,4
+    call readDisk
+    jmp LOADER_BASE_ADDR + 0x300
+
+readDisk:
+    mov esi,eax
+    mov di,cx
+
+;设置要读取的扇区数
+    mov dx,0x1f2
+    mov al,cl
+    out dx,al
+
+    mov eax,esi
+;设置LBA地址
+    mov dx,0x1f3
+    out dx,al
+
+    mov cl,8
+    shr eax,cl
+    mov dx,0x1f4
+    out dx,al
+
+    shr eax,cl
+    mov dx,0x1f5
+    out dx,al
+
+    shr eax,cl
+    and al,0x0f
+    or al,0xe0
+    mov dx,0x1f6
+    out dx,al
+;向0x1f7端口写命令，0x20
+    mov dx,0x1f7
+    mov al,0x20
+    out dx,al
+;监测硬件状态
+@notReady:
+    nop 
+    in al,dx
+    and al,0x88
+    cmp al,0x08
+    jnz @notReady
+;从0x1f0端口读取数据
+    mov ax,di
+    mov dx,256
+    mul dx
+    mov cx,ax
+
+    mov dx,0x1f0
+@goOnRead:
+    in ax,dx
+    mov [bx],ax
+    add bx,2
+    loop @goOnRead
+
+    ret
 
 message db "I'm mbr!Welcome come to ukernel!"
 end:
     jmp near $
     times 510-($-$$) db 0
     db 0x55,0xaa
-
-
-
-
-
-
-
-
-
-
-
 
